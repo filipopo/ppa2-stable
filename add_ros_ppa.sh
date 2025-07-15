6903e7e4894fa3d0d6199a17d71abcfeed42f617
cturmrsbot <cturmrsbot@users.noreply.github.com>
2025-07-15 12:38:17 +0000

    deploy: 644b660f3173f0373f38be13a8f3f97dfd5ff005

---
+++ add_ros_ppa.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+echo "$0: Adding ROS2 PPA"
+
+sudo apt-get -y update
+sudo apt-get -y install curl
+
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt-get -y update
+
+echo "$0: ROS2 PPA added"
